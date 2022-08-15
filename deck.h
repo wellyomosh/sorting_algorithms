d17c6ad62a8196c488700492e30ec5323862dd2c
wellyomosh <wellyomosh@gmail.com>
2022-08-15 05:07:11 -0700

    My algorithimic commit

--- deck.h
+++ deck.h
@@ -1,13 +1,21 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include <stdlib.h>
 
+/**
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -19,8 +27,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -32,13 +40,11 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
-
 void sort_deck(deck_node_t **deck);
-
 
 #endif /* DECK_H */
