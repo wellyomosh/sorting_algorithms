28fb441278134d9f1b7ecab7918c8538ab7a942c
wellyomosh <wellyomosh@gmail.com>
2022-08-15 04:05:31 -0700

    My algorithimic commit

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,47 +1,75 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers in ascending
- * order using the Counting sort algorithm
- *
- * @array: The array to be sorted.
- * @size: The size of the array.
- *
- */
+* get_max - Finds the maximum value in an array
+* @array: array to find max value of
+* @size: Size of array
+* Return: Largest value
+*/
+
+int get_max(int *array, size_t size)
+{
+	int i, max = 0;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		if (max < array[i])
+			max = array[i];
+	}
+	return (max);
+}
+
+/**
+* count - Counts number of occurences of value in an array
+* @array: Array to count values of
+* @size: Size of array
+* @val: Value to count in the array
+* Return: Count of va
+*/
+
+int count(int *array, size_t size, int val)
+{
+	int c = 0, i;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		if (array[i] == val)
+			c++;
+	}
+	return (c);
+}
+
+/**
+* counting_sort - sorts array using counting algorithm
+* @array: Array to sort
+* @size: Size of array
+*/
 
 void counting_sort(int *array, size_t size)
 {
-	size_t i = 1;
-	int max, j;
-	int *counter, *temp;
+	int max, *ca, i, *out, j;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
-
-	max = array[0];
-
-	for (; i < size; i++)
+	max = get_max(array, size);
+	out = malloc(sizeof(int) * (int)size);
+	ca = malloc(sizeof(int) * (max + 1));
+	if (ca == NULL || out == NULL)
+		return;
+	for (i = j = 0; i < max + 1; i++)
 	{
-		if (array[i] > max)
-			max = array[i];
+		j += count(array, size, i);
+		ca[i] = j;
 	}
-	counter = malloc(sizeof(int) * max + 1);
-	temp = malloc(sizeof(int) * size);
+	print_array(ca, max + 1);
+	for (i = 0; i < (int)size; i++)
+	{
+		out[ca[array[i]] - 1] = array[i];
+		ca[array[i]] -= 1;
+	}
+	for (i = 0; i < (int)size; i++)
+		array[i] = out[i];
 
-	for (i = 0; i < size; i++)
-		counter[array[i]] += 1;
-
-	for (j = 1; j <= max; j++)
-		counter[j] = counter[j] + counter[j - 1];
-
-	print_array(counter, max + 1);
-
-	for (i = 0; i < size; i++)
-		temp[counter[array[i] - 1]] = array[i];
-
-	for (i = 0; i < size; i++)
-		array[i] = temp[i];
-
-	free(temp);
-	free(counter);
+	free(out);
+	free(ca);
 }
