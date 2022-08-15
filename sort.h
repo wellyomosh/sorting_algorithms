01a42c90968a33aa1e4457775480e3b7fb296cc2
wellyomosh <wellyomosh@gmail.com>
2022-08-15 04:22:31 -0700

    My algorithimic commit

--- sort.h
+++ sort.h
@@ -1,21 +1,6 @@
-#ifndef SORT_H
-#define SORT_H
-
-/*
- * File: sort.h
- *
- * Desc: This header file contains all the function
- * prototpes and structure definations used in the
- * sorting algorithm project.
- *
- * Author: Gizachew Bayness (Elec Crazy)
- *
- * Date Created: Aug 13 2022
- *
- */
-
+#ifndef __SORT__
+#define __SORT__
 #include <stdlib.h>
-
 
 /**
  * struct listint_s - Doubly linked list node
@@ -23,9 +8,7 @@
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
- *
  */
-
 typedef struct listint_s
 {
 	const int n;
@@ -33,14 +16,22 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);
+/*======================================================================*/
+/*=================         Printing Algoritms        ==================*/
+/*======================================================================*/
+
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+/*======================================================================*/
+/*=================          sorting Algoritms        ==================*/
+/*======================================================================*/
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -49,5 +40,10 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
+void quick_s(int *array, int lo, int hi, size_t size);
+void merge(size_t lo, size_t mi, size_t hi, int *dest, int *src);
+void merge_partition(size_t lo, size_t hi, int *array, int *base);
+void swap_root(int *array, size_t root, size_t hi, size_t size);
 
-#endif
+#endif /* __SORT__ */
